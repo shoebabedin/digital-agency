e3e14e7bf45c720a83a8d72813eef0964d5d184c
shoebabedin <shoeb.abedin@intelli.global>
2022-10-23 13:08:41 +0600

    loader change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
   // preloader
-  $(window).on("load", function () {
-    $("#preloader ").fadeOut(500), $("#main").css({ overflow: "unset" });
-  });
+  // $(window).on("load", function () {
+  //   $("#preloader ").fadeOut(500), $("#main").css({ overflow: "unset" });
+  // });
+  $(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({'overflow':'visible'});
+  })
   // preloader
 
   // sticky navbar
